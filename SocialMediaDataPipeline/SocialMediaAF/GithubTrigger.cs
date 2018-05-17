dd693c9fe3b428450b81113f96e52f2f04bbe199
Benson Wang (Beyondsoft Consulting Inc) <v-bewan@microsoft.com>
2018-05-17 19:13:05 +0800

    azure func

---
+++ SocialMediaDataPipeline/SocialMediaAF/GithubTrigger.cs
@@ -0,0 +1,103 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using Microsoft.ServiceBus.Messaging;
+using Microsoft.ServiceBus;
+
+namespace SocialMediaAF
+{
+    public static class GithubTrigger
+    {
+        [FunctionName("GithubTrigger")]
+        public static void Run([TimerTrigger("0 * * * * *")]TimerInfo myTimer, TraceWriter log, ExecutionContext context, [ServiceBus("githubqueue", Connection = "socialmediasb")] out string msg)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(context.FunctionAppDirectory)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+            string dbStr = config.GetConnectionString("socialmedia");
+            string sbStr = config["socialmediasb"];
+
+            //Get message count in queue
+            var nsMGR = NamespaceManager.CreateFromConnectionString(sbStr);
+            long msgCount = nsMGR.GetQueue("githubqueue").MessageCount;
+             
+            DateTime maxUpdate = DateTime.MaxValue;
+            string status = string.Empty;
+            bool isNotified = false;
+            int jobRunId = 0;
+            using (SqlConnection conn = new SqlConnection(dbStr))
+            {
+                try
+                {
+                    conn.Open();
+                    var text = "SELECT TOP 1 MaxUpdate,Status,IsNotified,JobRunId FROM dbo.JobInfo "
+                            + "WHERE Platform='Github' ORDER BY JobRunId DESC";
+
+                    using (SqlCommand cmd = new SqlCommand(text, conn))
+                    {
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                maxUpdate = reader.GetDateTime(0);
+                                status = reader.GetString(1);
+                                isNotified = reader.GetBoolean(2);
+                                jobRunId = reader.GetInt32(3);
+                            }
+                        }
+                    }
+                    if (status == "Completed" && msgCount == 0)
+                    {
+
+                        maxUpdate = maxUpdate.AddHours(1);
+                        //update db status
+                        text = "INSERT INTO JobInfo " +
+                            "SELECT 'GitHub','" + maxUpdate + "','" + DateTime.UtcNow + "',null,'InProgress',0,null";
+                        using (SqlCommand cmd = new SqlCommand(text, conn))
+                        {
+                            cmd.ExecuteNonQuery();
+                        }
+                        //generate new message 
+                        msg = string.Format("{0};{1};{2}", "Start", maxUpdate, jobRunId + 1);
+                    }
+                    else if (status == "InProgress" && msgCount == 0 && isNotified == false)
+                    {
+                        text = "UPDATE JobInfo SET Status='Completed', IsNotified=1 WHERE JobRunId=" + jobRunId;
+                        using (SqlCommand cmd = new SqlCommand(text, conn))
+                        {
+                            cmd.ExecuteNonQuery();
+                        }
+                        //generate new message 
+                        msg = string.Format("{0};{1}", "NotifyComplete", string.Empty);
+                    }
+                    else if (status == "Failed" && msgCount != 0 && isNotified == false)
+                    {
+                        text = "UPDATE JobInfo SET Status='InProgress' WHERE JobRunId=" + jobRunId;
+                        using (SqlCommand cmd = new SqlCommand(text, conn))
+                        {
+                            cmd.ExecuteNonQuery();
+                        }
+                        msg = string.Format("{0};{1}", "ReactiveDeadMessage", string.Empty);
+                    }
+                    else
+                    {
+                        msg = string.Empty;
+                    }
+                }
+                catch (Exception e)
+                { 
+                    log.Info($"Throw Exception: {e.Message}");
+                    throw;
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
---
+++ SocialMediaDataPipeline/tracking.sql
@@ -0,0 +1,26 @@
+﻿DROP TABLE [JobInfo]
+CREATE TABLE [dbo].[JobInfo](
+	[JobRunId] int identity(1,1),
+	[Platform] [nvarchar](50) NULL,
+	[MaxUpdate] DATETIME NULL,
+	[JobStartAt] [datetime] NULL,
+	[JobEndAt] [datetime] NULL,
+    [Status] [nvarchar](20) NULL,
+	[IsNotified] bit null,
+	ErrorMessage nvarchar(max) null,
+ CONSTRAINT [PK_JobInfo] PRIMARY KEY CLUSTERED 
+(
+	[JobRunId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+)
+
+GO
+
+select GETUTCDATE()
+
+insert into jobinfo
+select 'Github','2018-05-16 23:00:00',GETUTCDATE(),DATEADD(day,1,getutcdate()),'Completed',1,null
+INSERT INTO JobInfo SELECT 'GitHub','5/17/2018 12:00:00 AM','5/17/2018 10:28:11 AM',null,'InProgress',0,null
+
+select * from jobinfo
+DELETE JOBINFO WHERE JOBRUNID=2
